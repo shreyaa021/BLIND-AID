6047d29510e2dd06e8ad00b8da10f7e566f6636d
shreyaa021 <shreyap.is17@bmsce.ac.in>
2021-01-09 20:27:45 +0530

    Settings corrected

--- app/src/main/java/com/example/controller/app/SettingsActivity.java
+++ app/src/main/java/com/example/controller/app/SettingsActivity.java
@@ -2,16 +2,14 @@ package com.example.controller.app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
 import android.widget.Switch;
 
 import com.example.controller.R;
 
 //TODO : may add FPS or Preview size too
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
-    Switch swc_autofocus, swc_flash, swc_multicolumn;
-    Spinner spr_ocr_lang;
+    Switch swc_autofocus , swc_flash ;
+
     int ocrLangPos;
     PreferenceManager manager;
     @Override
@@ -21,8 +19,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_settings);
         swc_autofocus = findViewById(R.id.swc_autofocus);
         swc_flash = findViewById(R.id.swc_flash);
-        swc_multicolumn = findViewById(R.id.swc_multicolumn);
-        spr_ocr_lang = findViewById(R.id.spr_lang);
+
         /*// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.arr_ocr_lang, android.R.layout.simple_spinner_item);
@@ -33,36 +30,17 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         getSettings();
         swc_autofocus.setOnClickListener(this);
         swc_flash.setOnClickListener(this);
-        swc_multicolumn.setOnClickListener(this);
-        String[] ocrLangCodes = getResources().getStringArray(R.array.arr_ocr_lang_code);
-        final CTextToSpeech tts = new CTextToSpeech(getApplicationContext(), ocrLangCodes[ocrLangPos]);
-        spr_ocr_lang.setSelection(ocrLangPos, true);
-        spr_ocr_lang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // TODO : check if tts lang available
 
-                if(tts.isLangAvailable(ocrLangCodes[position])) {
-                    ocrLangPos = position;
-                    manager.setOcrLangPos(position);
-                } else {
 
-                    spr_ocr_lang.setSelection(ocrLangPos);
-                }
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
+
     }
 
     private void getSettings() {
         swc_autofocus.setChecked(manager.getAutofocus());
         swc_flash.setChecked(manager.getFlash());
-        swc_multicolumn.setChecked(manager.getMulticolumn());
-        spr_ocr_lang.setSelection(manager.getOcrLangPos());
+
         ocrLangPos = manager.getOcrLangPos();
     }
 
@@ -75,9 +53,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             case R.id.swc_flash:
                 manager.setFlash(swc_flash.isChecked());
                 break;
-            case R.id.swc_multicolumn:
-                manager.setMulticolumn(swc_multicolumn.isChecked());
-                break;
+
         }
     }
 }
